0583a2be2c2e6d45235257eebba1964947e3f621
dinojan <dinojan@dinojans-Laptop.local>
2025-11-14 12:22:22 +0530

    add GetJobApplicaten api

--- jobfinder/jobs/serializers.py
+++ jobfinder/jobs/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
 from .models import *
 
-class GetJobListSerilizers(serializers.ModelSerializer):
+class GetJobListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model=Joblist
         fields='__all__'
+
+class GetApplyedJobSerializer(serializers.ModelSerializer):
+
+    user_applicant_details= serializers.SerializerMethodField()
+    
+    class Meta:
+        model=JobApplication
+        fields=['job', 'applicant_details', 'expection_rate', 'status','user_applicant_details']
+
+    def get_user_applicant_details(self, obj):
+        return{"user":obj.applicant_details.user.username, "is_job_seeker":obj.applicant_details.authentication.is_job_seeker, 
+               "is_client":obj.applicant_details.authentication.is_client,"is_contractor":obj.applicant_details.authentication.is_contractor}
